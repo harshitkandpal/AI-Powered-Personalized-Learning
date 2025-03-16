cd30a7a2dbe3159e4c97852bb7ab2bff9dfe4982
tanmaychavan14 <139693308+tanmaychavan14@users.noreply.github.com>
2025-03-16 20:10:47 +0530

    setup

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\harshit kandpal\Desktop\AI-Powered-Personalized-Learning\backend\venv
+set VIRTUAL_ENV=E:\Hack to Crack 2025 Vishawniketan\Hacktocrack2.0\AI-Powered-Personalized-Learning\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
